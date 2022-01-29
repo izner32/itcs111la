987b83f07cb3c8ef2fab4e43f026e2952b7e0800
Renz <renzcarillo32@gmail.com>
2022-01-29 22:23:30 +0800

    feat: added footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,7 +4,28 @@ import Image from 'next/image';
 function Footer() {
   return (
     <>
-      
+      <div className="text-xs px-8 sm:px-16 md:px-24 lg:px-32 py-6 font-light text-white text-sm sm:text-base flex justify-between bg-ironside-gray-500">
+        <ul className="flex">
+          <li className="mr-4">
+            <a className="hover:text-black" href="/">Home</a>
+          </li>
+          <li className="mr-4">
+            <a className="hover:text-black" href="/devs/albios">Albios</a>
+          </li>
+          <li className="mr-4">
+            <a className="hover:text-black" href="/devs/carillo">Carillo</a>
+          </li>
+          <li className="mr-4">
+            <a className="hover:text-black" href="/devs/delgado">Delgado</a>
+          </li>
+          <li className="mr-4">
+            <a className="hover:text-black" href="/devs/waje">Waje</a>
+          </li>
+        </ul>
+        <div className="font-light">
+          <p>© DLSUDevs. All rights reserved </p>
+        </div>
+      </div>
     </>
   )
 }
--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -6,7 +6,7 @@ function Nav() {
     <>
       <div className="px-32 py-4 sm:py-6 md:py-8 lg:py-10 lg:pb-12 text-center justify-between md:flex ">
         <div className="text-sm sm:text-base md:text-lg lg:text-xl mr-3 mb-4 md:mb-0">
-          <a className="inline-block rounded py-1 px-3 font-bold" href="#">DLSUDevs</a>
+          <a className="inline-block rounded py-1 px-3 font-bold" href="/">DLSUDevs</a>
         </div>
         <ul className="text-xs sm:text-sm md:text-base lg:text-lg flex justify-center">
           <li className="mr-3">
