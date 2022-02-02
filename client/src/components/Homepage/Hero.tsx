c122b86454a7a6b29d8e568ad30c4b17f859bae2
Renz <renzcarillo32@gmail.com>
2022-02-02 16:06:05 +0800

    feat: added hero section (mayo's work)

--- client/src/components/Homepage/Hero.tsx
+++ client/src/components/Homepage/Hero.tsx
@@ -4,7 +4,64 @@ import styles from './hero.module.css';
 function Hero() {
   return (
     <>
-      
+      <div id={styles.main_box} className="flex-col-reverse md:flex-row">
+        <div id={styles.left_box} className="md:w-[50%]">
+          <div id={styles.final_project} className="flex flex-col mx-auto my-16 text-left w-[80%] ">
+            <h1 className="font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl mb-4">FINAL<br></br>PROJECT</h1>
+            <p className="text-xs sm:text-sm md:text-base lg:text-lg mb-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <button className="transition ease-in-out delay-150 hover:-translate-y-1 hover:bg-ironside-gray-200 p-2 px-4 rounded text-white duration-300 bg-foggy-gray-500 p-3 rounded w-max text-xs sm:text-xs md:text-sm lg:text-base font-semibold p-0 m-0">Randomly&nbsp;Meet&nbsp;One&nbsp;of&nbsp;Us!</button>
+          </div>
+        </div>
+
+        <div className="md:w-[50%] relative top-0 z-20 bg-foggy-gray-500">
+          <div className="hidden md:flex h-full">  
+          <div className="img_1 ">
+            <div 
+              className="p-10 relative right-10 top-20"
+              style={{  
+                  backgroundImage: "url(" + "User_1.svg" + ")",
+                  backgroundPosition: 'center',
+                  backgroundSize: 'cover',
+                  backgroundRepeat: 'no-repeat'
+                }}
+            ></div>
+          </div>
+          <div className="img_2 mx-auto">
+            <div 
+              className="p-8 relative top-[200px]"
+              style={{  
+                  backgroundImage: "url(" + "User_3.svg" + ")",
+                  backgroundPosition: 'center',
+                  backgroundSize: 'cover',
+                  backgroundRepeat: 'no-repeat'
+                }}
+            ></div>
+          </div>
+          <div className="img_3 mx-auto">
+            <div 
+              className="p-16 relative top-20"
+              style={{  
+                  backgroundImage: "url(" + "User_2.svg" + ")",
+                  backgroundPosition: 'center',
+                  backgroundSize: 'cover',
+                  backgroundRepeat: 'no-repeat'
+                }}
+            ></div>
+          </div>
+          <div className="img_4 mx-auto">
+            <div 
+              className="p-12  relative top-[250px]"
+              style={{  
+                  backgroundImage: "url(" + "User_4.svg" + ")",
+                  backgroundPosition: 'center',
+                  backgroundSize: 'cover',
+                  backgroundRepeat: 'no-repeat'
+                }}
+            ></div>
+          </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- client/src/components/Homepage/Logo.tsx
+++ client/src/components/Homepage/Logo.tsx
@@ -4,7 +4,41 @@ import styles from './logo.module.css';
 function Logo() {
   return (
     <>
-      
+      <div className="">
+        <div className="text-center">
+          <h1 className="text-base sm:text-lg md:text-xl lg:text-2xl font-bold mb-4"> Built for </h1>
+          <h1>Final Project in ITCS111LA</h1>
+          <div >
+              <img className="mx-auto" src="https://www.dlsud.edu.ph/images/logo.png" alt="logo.png" width="400" height="100"></img>
+          </div>
+        </div>
+
+        <div >
+          <h1>Who are we?</h1>
+        </div>
+        <div className="">
+          <div>
+            <h1>Albios, Shahid</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sodales tristique sapien, id lobortis odio convallis vitae. Duis vehicula enim in condimentum consectetur. Fusce a magna eget augue gravida maximus. Etiam dapibus nisi at massa rhoncus, sed aliquam nulla finibus. Duis urna erat, placerat a euismod pretium, fermentum sed lacus. Morbi non urna vitae ante ornare blandit eget sed mauris. Fusce consequat nisi nec leo vestibulum euismod. Sed hendrerit, elit et varius vestibulum, ante neque consequat nulla, et pretium purus diam in orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae...</p>
+            <button>Explore →</button>
+          </div>
+          <div>
+            <h1>Albios, Shahid</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sodales tristique sapien, id lobortis odio convallis vitae. Duis vehicula enim in condimentum consectetur. Fusce a magna eget augue gravida maximus. Etiam dapibus nisi at massa rhoncus, sed aliquam nulla finibus. Duis urna erat, placerat a euismod pretium, fermentum sed lacus. Morbi non urna vitae ante ornare blandit eget sed mauris. Fusce consequat nisi nec leo vestibulum euismod. Sed hendrerit, elit et varius vestibulum, ante neque consequat nulla, et pretium purus diam in orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae...</p>
+            <button>Explore →</button>
+          </div>
+          <div>
+            <h1>Albios, Shahid</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sodales tristique sapien, id lobortis odio convallis vitae. Duis vehicula enim in condimentum consectetur. Fusce a magna eget augue gravida maximus. Etiam dapibus nisi at massa rhoncus, sed aliquam nulla finibus. Duis urna erat, placerat a euismod pretium, fermentum sed lacus. Morbi non urna vitae ante ornare blandit eget sed mauris. Fusce consequat nisi nec leo vestibulum euismod. Sed hendrerit, elit et varius vestibulum, ante neque consequat nulla, et pretium purus diam in orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae...</p>
+            <button>Explore →</button>
+          </div>
+          <div>
+            <h1>Albios, Shahid</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sodales tristique sapien, id lobortis odio convallis vitae. Duis vehicula enim in condimentum consectetur. Fusce a magna eget augue gravida maximus. Etiam dapibus nisi at massa rhoncus, sed aliquam nulla finibus. Duis urna erat, placerat a euismod pretium, fermentum sed lacus. Morbi non urna vitae ante ornare blandit eget sed mauris. Fusce consequat nisi nec leo vestibulum euismod. Sed hendrerit, elit et varius vestibulum, ante neque consequat nulla, et pretium purus diam in orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae...</p>
+            <button>Explore →</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
