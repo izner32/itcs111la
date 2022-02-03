5ebf7174fa79b0b557c45f31de61bda46d81ab24
Renz <renzcarillo32@gmail.com>
2022-02-03 08:12:02 +0800

    update: updated responsive of footer and devs description

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 function Footer() {
   return (
     <>
-      <div className="px-8 sm:px-16 md:px-24 lg:px-32 py-6 font-light text-white text-sm sm:text-base flex justify-between bg-ironside-gray-500">
-        <ul className="flex text-[0.60rem] sm:text-sm">
+      <div className="px-8 sm:px-16 md:px-24 lg:px-32 py-6 font-light text-white text-sm sm:text-base flex flex-col text-center  md:flex-row md:justify-between bg-ironside-gray-500">
+        <ul className="flex justify-center text-[0.60rem] sm:text-sm">
           <li className="mr-4">
             <a className="hover:text-black" href="/">Home</a>
           </li>
